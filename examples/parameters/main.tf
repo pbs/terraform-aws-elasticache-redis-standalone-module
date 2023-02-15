92a6c2b508b0764486f927f20b0a6fb39ee82a53
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2023-02-15 10:20:22 -0500

    Fixing parameters example

--- examples/parameters/main.tf
+++ examples/parameters/main.tf
@@ -3,12 +3,13 @@ module "redis" {
 
   private_hosted_zone = var.private_hosted_zone
 
+  parameter_group_name = module.parameters.name
+
   organization = var.organization
   environment  = var.environment
   product      = var.product
   repo         = var.repo
 }
-
 
 module "parameters" {
   source = "github.com/pbs/terraform-aws-elasticache-parameter-group-module?ref=0.0.1"
